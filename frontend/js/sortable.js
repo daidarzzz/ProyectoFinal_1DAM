7a1ab4f49d873035b28a795fd7024d2b0a40c1a6
daidarzzz <penealcachofa882@gmail.com>
2025-12-10 19:08:33 +0100

    mensaje para alguien

---
+++ frontend/js/sortable.js
@@ -0,0 +1,13 @@
+   new Sortable(document.getElementById('prueba-listado'), {
+  animation: 150
+});
+
+            new Sortable(document.getElementById('prueba-hola'), {
+  animation: 11150
+});
+
+
+
+function hola() {
+    console.log("Hola");
+}
